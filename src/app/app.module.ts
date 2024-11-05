60ae6ef442076853a7a2b894bf652172149fd3cc
aseasigal <asea301998@gmail.com>
2024-11-05 04:15:11 -0300

    primer Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListClientsComponent } from './customers/components/list-clients/list-clients.component';
+import { FormClientsComponent } from './customers/components/form-clients/form-clients.component';
+import { FormProductComponent } from './products/components/form-product/form-product.component';
+import { ListProductComponent } from './products/components/list-product/list-product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListClientsComponent,
+    FormClientsComponent,
+    FormProductComponent,
+    ListProductComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/customers/components/form-clients/form-clients.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'form-clients',
+  templateUrl: './form-clients.component.html',
+  styleUrl: './form-clients.component.css'
+})
+export class FormClientsComponent {
+
+}
